5af94bcc85bef0de6850a18b5bc0c3a4d73b4d31
Maria Elena <111201881+MariaElenaMas@users.noreply.github.com>
2023-09-09 13:19:52 +0200

    Files updated

--- src/dist/index.0d6fcb10.js
+++ src/dist/index.0d6fcb10.js
@@ -142,13 +142,13 @@
       this[globalName] = mainExports;
     }
   }
-})({"gOaZ4":[function(require,module,exports) {
+})({"c7kMa":[function(require,module,exports) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
-module.bundle.HMR_BUNDLE_ID = "022c1b16b4b6dfad";
+module.bundle.HMR_BUNDLE_ID = "10485a0f0d6fcb10";
 "use strict";
 /* global HMR_HOST, HMR_PORT, HMR_ENV_HASH, HMR_SECURE, chrome, browser, __parcel__import__, __parcel__importScripts__, ServiceWorkerGlobalScope */ /*::
 import type {
@@ -27303,6 +27303,6 @@ function registerExportsForReactRefresh(module1) {
     }
 }
 
-},{"5d9c5f27982f8f05":"b8uOU"}]},["gOaZ4","34P0T","d8Dch"], "d8Dch", "parcelRequireaec4")
+},{"5d9c5f27982f8f05":"b8uOU"}]},["c7kMa","34P0T","d8Dch"], "d8Dch", "parcelRequireaec4")
 
-//# sourceMappingURL=index.b4b6dfad.js.map
+//# sourceMappingURL=index.0d6fcb10.js.map
