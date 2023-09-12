57827e135882d1c40b3ed4dd829158b338f0ed66
Maria Elena <111201881+MariaElenaMas@users.noreply.github.com>
2023-09-13 01:19:12 +0200

    propTypes added

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -6,4 +8,23 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.Title}
     </div>
   );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Image: PropTypes.string.isRequired,
+    Year: PropTypes.string.isRequired,
+  }).isRequired,
+  Genre: PropTypes.arrayOf(
+    PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  Directors: PropTypes.shape({
+    Name: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
