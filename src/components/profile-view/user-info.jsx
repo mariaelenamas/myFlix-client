e66299f1a900a4c51934847d45ea7cbe4187e594
Maria Elena <111201881+MariaElenaMas@users.noreply.github.com>
2023-10-12 22:27:21 +0200

    Change date format

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 export const UserInfo = ({ user }) => {
+    const date = () => {
+        let BD = new Date(user.Birthday)
+        return BD.toLocaleDateString()
+    }
     return (
         <>
             <p>User : {user.Username} </p>
             <p>Email: {user.Email} </p>
-            <p>Date of birth: {user.Birthday} </p>
+            <p>Date of birth: {date()} </p>
         </>
     );
 };
